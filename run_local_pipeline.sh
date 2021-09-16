c9982735f83443258d809da1294d7de5db96011c
Damian Straszak <damian.straszak@gmail.com>
2021-09-16 13:14:08 +0200

    Upgraded alephbft. (#126)

--- run_local_pipeline.sh
+++ run_local_pipeline.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-CARGO_INCREMENTAL=0 cargo +nightly clippy --all-targets --all-features
-CARGO_INCREMENTAL=0 cargo +nightly fmt --all
+CARGO_INCREMENTAL=0 cargo +nightly-2021-07-21 clippy --all-targets --all-features
+CARGO_INCREMENTAL=0 cargo +nightly-2021-07-21 fmt --all
 CARGO_INCREMENTAL=0 cargo test --lib
