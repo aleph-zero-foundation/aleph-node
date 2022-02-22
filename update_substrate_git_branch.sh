3749e3dfceae145caf603b1567c97da5a3b11260
kostekIV <27210860+kostekIV@users.noreply.github.com>
2022-02-22 21:14:54 +0100

    Modify script to change branch instead of rev (#266)

--- update_substrate_git_branch.sh
+++ update_substrate_git_branch.sh
@@ -2,15 +2,14 @@
 
 set -e
 
-rev=$(git ls-remote https://github.com/paritytech/substrate.git | grep refs/heads/master | cut -c 1-7)
-
+branch="$1"
 
 paths=(bin/node/Cargo.toml bin/runtime/Cargo.toml finality-aleph/Cargo.toml
     primitives/Cargo.toml pallet/Cargo.toml)
 
 for path in ${paths[@]}; do
     echo upgrade $path
-    sed -e 's/\(substrate.git.*rev = "\).*"/\1'$rev'"/' < $path > x
+    sed -e 's/\(substrate.git.*branch = "\)[^"]*".*\([,}]\)/\1'$branch'" \2/' < $path > x
     mv x $path
 done
 
