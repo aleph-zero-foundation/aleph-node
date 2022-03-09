54570b9af4dc4cc086a8cbe2aa3dcc891a6deeef
kostekIV <27210860+kostekIV@users.noreply.github.com>
2022-03-09 09:45:35 +0100

    adjust path (#329)

--- scripts/run_local_e2e_pipeline.sh
+++ scripts/run_local_e2e_pipeline.sh
@@ -10,7 +10,7 @@ docker build --tag aleph-node:latest -f ./docker/Dockerfile .
 # run the chain and the tests in two separate tmux windows
 tmux new-session -d -s aleph0 './.github/scripts/run_consensus.sh';
 tmux new-window -t "aleph0:1";
-tmux send-keys -t "aleph0:1" './run_e2e.sh' Enter;
+tmux send-keys -t "aleph0:1" './scripts/run_e2e.sh' Enter;
 
 tmux a;
 
