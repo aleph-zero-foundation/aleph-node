0207b02f5af5986e0c20cfb83f9ee23923185ffc
Piotr Mikołajczyk <piomiko41@gmail.com>
2022-05-13 09:24:25 +0200

    Fix `update_substrate_git_branch.sh` (#418)

--- scripts/update_substrate_git_branch.sh
+++ scripts/update_substrate_git_branch.sh
@@ -4,12 +4,16 @@ set -e
 
 branch="$1"
 
-paths=(bin/node/Cargo.toml bin/runtime/Cargo.toml finality-aleph/Cargo.toml
-    primitives/Cargo.toml pallet/Cargo.toml)
+# Find all `Cargo.toml` files outside any `target` directory.
+paths=$(find . -mindepth 2 -type f -name "Cargo.toml" -not -path "*/target/*")
 
 for path in ${paths[@]}; do
     echo upgrade $path
-    sed -e 's/\(substrate.git.*branch = "\)[^"]*".*\([,}]\)/\1'$branch'" \2/' < $path > x
+    # 1. Find and capture `paritytech/substrate.git", branch = "` substring. It will be available as `\1`. In place
+    #    of spaces there can be sequence of `\s` characters.
+    # 2. Find and capture whatever is after closing `"` and before `,` or `}`. It will be available as `\2`.
+    # 3. Substitute new branch and concatenate it with `\1` and `\2`.
+    sed -e 's/\(paritytech\/substrate.git"\s*,\s*branch\s*=\s*"\)[^"]*"\([^,}]*\)/\1'$branch'"\2/' < $path > x
     mv x $path
 done
 
