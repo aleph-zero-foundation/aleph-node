c192be3b7e890ac25a3722dc24e7a3d494a07689
skrolikowski-10c <84325795+skrolikowski-10c@users.noreply.github.com>
2022-05-25 15:13:31 +0200

    Added sleep for the entrypoint and additional tag for image pushed from workflow (#443)
    Co-authored-by: Szymon Królikowski <s.krolikowski@tenesys.pl>

--- docker-runtime-hook/entrypoint.sh
+++ docker-runtime-hook/entrypoint.sh
@@ -9,6 +9,9 @@ SUDO_PHRASE=${RUNTIME_PHRASE}
 RPC_ADDR="${RPC_ENVIRONMENT_ENDPOINT:-rpc.dev.azero.dev}"
 WS_ADDR="${WS_ENVIRONMENT_ENDPOINT:-ws.dev.azero.dev}"
 
+echo "Heating up for 10s"
+sleep 10
+
 echo -n  $(date +"%d-%b-%y %T") "   Checking runtime version on devnet: "
 OLD_VER=$(curl -sS -H "Content-Type: application/json" -d '{"id":1, "jsonrpc":"2.0", "method": "state_getRuntimeVersion"}' $RPC_ADDR | jq .result.specVersion)
 echo "$OLD_VER"
