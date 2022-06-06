1d79923480ee6c3e8e8896b57fb920c268497e68
kostekIV <27210860+kostekIV@users.noreply.github.com>
2022-06-06 17:47:06 +0200

    fix script cmd (#467)

--- scripts/run_e2e.sh
+++ scripts/run_e2e.sh
@@ -4,6 +4,6 @@ set -e
 
 cd e2e-tests/
 
-RUST_LOG=aleph_e2e_client=info,aleph-client=info cargo run -- --node 127.0.0.1:9943 --test-cases members_rotate
+RUST_LOG=aleph_e2e_client=info,aleph-client=info cargo run -- --node 127.0.0.1:9943
 
 exit $?
