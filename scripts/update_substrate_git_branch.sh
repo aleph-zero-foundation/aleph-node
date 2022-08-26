6dbe01fc4a4c823630487838265de78fc6680953
Piotr Mikołajczyk <piomiko41@gmail.com>
2022-08-26 12:14:51 +0200

    Improve script for updating substrate branch. Update Cargo.locks (#602)

--- scripts/update_substrate_git_branch.sh
+++ scripts/update_substrate_git_branch.sh
@@ -1,20 +1,34 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-set -e
+set -euo pipefail
 
-branch="$1"
+function usage(){
+  cat << EOF
+Substitutes the branch name of the repository `https://github.com/Cardinal-Cryptography/substrate` in all Cargo.toml files.
+
+Usage:
+  $0 <new_branch_name>
+EOF
+  exit 0
+}
+
+BRANCH="${1:-}"
+if [[ -z "${BRANCH}" ]]; then
+       usage
+       exit 2
+fi
 
 # Find all `Cargo.toml` files outside any `target` directory.
-paths=$(find . -mindepth 2 -type f -name "Cargo.toml" -not -path "*/target/*")
+paths=$(find . -mindepth 2 -type f -name "Cargo.toml" -not -path "*/target/*") || echo "Problems with finding Cargo.toml files"
 
 for path in ${paths[@]}; do
-    echo upgrade $path
-    # 1. Find and capture `Cardinal-Cryptography/substrate.git", branch = "` substring. It will be available as `\1`. In place
-    #    of spaces there can be sequence of `\s` characters.
-    # 2. Find and capture whatever is after closing `"` and before `,` or `}`. It will be available as `\2`.
-    # 3. Substitute new branch and concatenate it with `\1` and `\2`.
-    sed -e 's/\(Cardinal-Cryptography\/substrate.git"\s*,\s*branch\s*=\s*"\)[^"]*"\([^,}]*\)/\1'$branch'"\2/' < $path > x
-    mv x $path
+    echo "Upgrading ${path}"
+    # 1. Filter out lines not containing `https://github.com/Cardinal-Cryptography/substrate[.git]"`.
+    # 2. Substitute `###` in `branch = "###"` with $BRANCH.
+    sed -e '/https:\/\/github.com\/Cardinal-Cryptography\/substrate\(.git\)\{0,1\}"/s/\(branch\s*=\s*"\)[^"]*"\([^,}]*\)/\1'"${BRANCH//\//\\/}"'"\2/' < $path > x
+    mv x "${path}"
 done
 
 cargo update
+
+exit 0
