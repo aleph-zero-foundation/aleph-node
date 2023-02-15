e839ba05ec136bd3fd61c90c456d0df82976f964
Marcin <marcin.radecki@cardinals.cc>
2023-02-15 12:53:19 +0100

    Bumped spec_version to 50

--- bin/runtime/src/lib.rs
+++ bin/runtime/src/lib.rs
@@ -105,7 +105,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("aleph-node"),
     impl_name: create_runtime_str!("aleph-node"),
     authoring_version: 1,
-    spec_version: 49,
+    spec_version: 50,
     impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 14,
