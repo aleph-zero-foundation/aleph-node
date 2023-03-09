79689b687cd8effd61983440b3307b54a576dbd8
deuszx <95355183+deuszx@users.noreply.github.com>
2023-03-09 17:54:38 +0100

    Fail early with helpful error message. (#972)
    Co-authored-by: fixxxedpoint <lukasz.lachowski@cardinals.cc>

--- .github/scripts/run_consensus.sh
+++ .github/scripts/run_consensus.sh
@@ -17,6 +17,15 @@ MIN_VALIDATOR_COUNT=4
 
 export NODE_IMAGE
 
+if docker inspect ${NODE_IMAGE} > /dev/null ; then
+  echo "${NODE_IMAGE} found locally"
+else
+  echo "${NODE_IMAGE} not found locally."
+  echo "Build image first with:"
+  echo "docker build -t ${NODE_IMAGE} -f docker/Dockerfile ."
+  exit 1;
+fi
+
 mkdir -p docker/data/
 
 function usage {
