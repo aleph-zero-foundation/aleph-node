f889600d4e1075cffd8a2d0e2002f572692a3570
kostekIV <27210860+kostekIV@users.noreply.github.com>
2023-05-16 11:19:27 +0200

    add migrations (#1185)

--- bin/runtime/src/lib.rs
+++ bin/runtime/src/lib.rs
@@ -34,7 +34,7 @@ use frame_system::{EnsureRoot, EnsureSignedBy};
 #[cfg(feature = "try-runtime")]
 use frame_try_runtime::UpgradeCheckSelect;
 pub use pallet_balances::Call as BalancesCall;
-use pallet_committee_management::SessionAndEraManager;
+use pallet_committee_management::{PrefixMigration, SessionAndEraManager};
 pub use pallet_timestamp::Call as TimestampCall;
 use pallet_transaction_payment::{CurrencyAdapter, Multiplier, TargetedFeeAdjustment};
 pub use primitives::Balance;
@@ -409,6 +409,7 @@ parameter_types! {
     pub const MaxPointsToBalance: u8 = 10;
 }
 
+use pallet_elections::{CommitteeSizeMigration, MigrateToV4};
 use sp_runtime::traits::Convert;
 
 pub struct BalanceToU256;
@@ -846,6 +847,13 @@ pub type UncheckedExtrinsic =
     generic::UncheckedExtrinsic<Address, RuntimeCall, Signature, SignedExtra>;
 /// Extrinsic type that has already been checked.
 pub type CheckedExtrinsic = generic::CheckedExtrinsic<AccountId, RuntimeCall, SignedExtra>;
+
+pub type Migrations = (
+    PrefixMigration<Runtime>,
+    MigrateToV4<Runtime>,
+    CommitteeSizeMigration<Runtime>,
+);
+
 /// Executive: handles dispatch to the various modules.
 pub type Executive = frame_executive::Executive<
     Runtime,
@@ -853,6 +861,7 @@ pub type Executive = frame_executive::Executive<
     frame_system::ChainContext<Runtime>,
     Runtime,
     AllPalletsWithSystem,
+    Migrations,
 >;
 
 #[cfg(feature = "runtime-benchmarks")]
