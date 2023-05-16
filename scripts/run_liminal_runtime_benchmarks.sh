470641cd967ec452f4a98bd068bb23184aa8156e
Piotr Mikołajczyk <piomiko41@gmail.com>
2023-05-16 09:02:07 +0200

    A0-2494: Create benchmarking script (#1183)
    * Create benchmarking script
    
    * Newline

---
+++ scripts/run_liminal_runtime_benchmarks.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -euo pipefail
+
+export NODE_ID=5D34dL5prEUaGNQtPPZ3yN5Y6BnkfXunKXXz6fo7ZJbLwRRH
+
+cargo run --release -p aleph-node --features liminal-runtime-benchmarks -- bootstrap-chain \
+    --base-path /tmp/ \
+    --account-ids $NODE_ID \
+    --sudo-account-id $NODE_ID \
+    --chain-id a0snarknet \
+    --token-symbol SNRKZERO \
+    --chain-name 'Aleph Zero Snarknet' \
+    > ./chainspec.json
+
+cargo run --release -p aleph-node --features liminal-runtime-benchmarks -- benchmark pallet \
+    --chain=chainspec.json \
+    --pallet=pallet_baby_liminal \
+    --extrinsic='*' \
+    --steps=20 \
+    --repeat=5 \
+    --template=.maintain/pallet-weight-template.hbs \
+    --execution=wasm \
+    --wasm-execution=compiled \
+    --output=pallets/baby-liminal/src/weights.rs
