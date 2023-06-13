0d2780afda56220aa21fa443cd322e44ec4c3396
timorl <tymorl@gmail.com>
2023-06-13 11:07:13 +0200

    Do not treat these blocks like that (#1252)
    # Description
    
    Remove a leftover change cherrypicked from a release candidate that did
    not end up working properly.
    
    ## Type of change
    
    - Bug fix (non-breaking change which fixes an issue)
    
    # Checklist:
    
    Co-authored-by: timorl <timorl@disroot.org>

--- finality-aleph/src/import.rs
+++ finality-aleph/src/import.rs
@@ -1,11 +1,11 @@
 use std::{fmt::Debug, time::Instant};
 
 use futures::channel::mpsc::{TrySendError, UnboundedSender};
-use log::{debug, trace, warn};
+use log::{debug, warn};
 use sc_consensus::{
     BlockCheckParams, BlockImport, BlockImportParams, ImportResult, JustificationImport,
 };
-use sp_consensus::{BlockOrigin, Error as ConsensusError};
+use sp_consensus::Error as ConsensusError;
 use sp_runtime::{
     traits::{Block as BlockT, Header},
     Justification as SubstrateJustification,
@@ -175,10 +175,6 @@ where
         let post_hash = block.post_hash();
 
         let justifications = block.justifications.take();
-        if matches!(block.origin, BlockOrigin::NetworkInitialSync) {
-            trace!(target: "aleph-justification", "Treating block {:?} {:?} from major sync as from a normal sync.", number, block.header.hash());
-            block.origin = BlockOrigin::NetworkBroadcast;
-        }
 
         debug!(target: "aleph-justification", "Importing block {:?} {:?} {:?}", number, block.header.hash(), block.post_hash());
         let result = self.inner.import_block(block).await;
