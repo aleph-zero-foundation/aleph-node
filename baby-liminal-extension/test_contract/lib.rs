69cc26f6850a7101ce37ce8805c9ad0fb000cca5
Piotr Mikołajczyk <piomiko41@gmail.com>
2023-11-28 09:40:15 +0100

    A0-3509: Test verify API as well (#1514)
    # Description
    
    Frontend contract test was missing call to the second method of the
    extension. This PR fills the gap.
    
    ## Type of change
    
    - Bug fix (non-breaking change which fixes an issue)

--- baby-liminal-extension/test_contract/lib.rs
+++ baby-liminal-extension/test_contract/lib.rs
@@ -14,10 +14,18 @@ mod test_contract {
         }
 
         #[ink(message)]
-        pub fn call_ce(&self) {
+        pub fn call_store_key(&self) {
             self.env()
                 .extension()
                 .store_key(self.env().caller(), [0; 8], vec![0; 32])
+                .unwrap();
+        }
+
+        #[ink(message)]
+        pub fn call_verify(&self) {
+            self.env()
+                .extension()
+                .verify([0; 8], vec![0; 41], vec![0; 82])
                 .unwrap();
         }
     }
@@ -39,12 +47,27 @@ mod test_contract {
             }
         }
 
-        #[ink::test]
-        fn it_works() {
-            register_chain_extension(MockedStoreKeyExtension);
+        struct MockedVerifyExtension;
+        impl ink::env::test::ChainExtension for MockedVerifyExtension {
+            fn func_id(&self) -> u32 {
+                baby_liminal_extension::extension_ids::VERIFY_EXT_ID
+            }
 
-            let test_contract = TestContract::new();
-            test_contract.call_ce();
+            fn call(&mut self, _: &[u8], _: &mut Vec<u8>) -> u32 {
+                baby_liminal_extension::status_codes::VERIFY_SUCCESS
+            }
+        }
+
+        #[ink::test]
+        fn store_key_works() {
+            register_chain_extension(MockedStoreKeyExtension);
+            TestContract::new().call_store_key();
+        }
+
+        #[ink::test]
+        fn verify_works() {
+            register_chain_extension(MockedVerifyExtension);
+            TestContract::new().call_verify();
         }
     }
 }
