0fff20564b60920133ed13e64c61841d7c5675e8
Marcin <marcin.radecki@cardinals.cc>
2024-02-05 11:18:29 +0100

    A0-4009: Bump node and runtime spec_version and tx version in release-13 branch (#1603)
    # Description
    
    * bump `spec_version` and `transaction_version`
    * bump runtime and node version
    
    Current `spec_version` and `transaction_version` on Testnet in Mainet is
    the same and as follows: `68` and `17`.

--- bin/runtime/src/lib.rs
+++ bin/runtime/src/lib.rs
@@ -97,10 +97,10 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("aleph-node"),
     impl_name: create_runtime_str!("aleph-node"),
     authoring_version: 1,
-    spec_version: 67,
+    spec_version: 69,
     impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
-    transaction_version: 17,
+    transaction_version: 18,
     state_version: 0,
 };
 
