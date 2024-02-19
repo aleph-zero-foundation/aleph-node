e625d066f3257f32c6ddb95bc245a9ee3b579e52
woocash2 <59764862+woocash2@users.noreply.github.com>
2024-02-19 14:21:33 +0100

    A0-3589: Direct storage read functions (#1619)
    # Description
    
    An implementation of direct storage access to support
    backwards-compatible runtime API.
    * Added `read_storage_map` which reads the element of a runtime storage
    map directly
    * Improved error handling
    * Added test which checks that `read_storage_map` and
    `read_storage_value` work assuming appropriate storage key naming
    convention (map element is under appropriately digested version of
    `pallet + map_name + key`, storage value similarly at `pallet +
    storage_value_name`).
    
    ## Type of change
    - New feature (non-breaking change which adds functionality)
    
    ## Checklist:
    - I have added tests

--- finality-aleph/src/runtime_api.rs
+++ finality-aleph/src/runtime_api.rs
@@ -5,7 +5,8 @@ use std::{
 };
 
 use aleph_runtime::SessionKeys;
-use parity_scale_codec::{Decode, DecodeAll, Error as DecodeError};
+use frame_support::StorageHasher;
+use parity_scale_codec::{Decode, DecodeAll, Encode, Error as DecodeError};
 use sc_client_api::Backend;
 use sp_application_crypto::key_types::AURA;
 use sp_core::twox_128;
@@ -63,36 +64,74 @@ where
         }
     }
 
-    fn read_storage<D: Decode>(
+    fn access_storage<D: Decode>(
+        &self,
+        storage_key: Vec<u8>,
+        at_block: BlockHash,
+    ) -> Result<D, ApiError> {
+        let encoded = self
+            .client
+            .storage(at_block, &sc_client_api::StorageKey(storage_key))
+            .map_err(|_| ApiError::StorageAccessFailure)?
+            .ok_or(ApiError::NoStorage)?;
+        D::decode_all(&mut encoded.0.as_ref()).map_err(ApiError::DecodeError)
+    }
+
+    fn read_storage_value<D: Decode>(
         &self,
         pallet: &str,
         item: &str,
         at_block: BlockHash,
     ) -> Result<D, ApiError> {
         let storage_key = [twox_128(pallet.as_bytes()), twox_128(item.as_bytes())].concat();
+        match self.access_storage::<D>(storage_key, at_block) {
+            Err(ApiError::NoStorage) => Err(ApiError::NoStorageValue(pallet.into(), item.into())),
+            other => other,
+        }
+    }
 
-        let encoded = match self
-            .client
-            .storage(at_block, &sc_client_api::StorageKey(storage_key))
-        {
-            Ok(Some(e)) => e,
-            _ => return Err(ApiError::NoStorage(pallet.to_string(), item.to_string())),
-        };
-
-        D::decode_all(&mut encoded.0.as_ref()).map_err(ApiError::DecodeError)
+    #[allow(dead_code)]
+    fn read_storage_map<H: StorageHasher, D: Decode, E: Encode>(
+        &self,
+        pallet: &str,
+        item: &str,
+        key: E,
+        at_block: BlockHash,
+    ) -> Result<D, ApiError> {
+        let mut storage_key = [twox_128(pallet.as_bytes()), twox_128(item.as_bytes())].concat();
+        let hashed_encoded_key = key.using_encoded(H::hash);
+        storage_key.extend(hashed_encoded_key.as_ref());
+        match self.access_storage::<D>(storage_key, at_block) {
+            Err(ApiError::NoStorage) => {
+                Err(ApiError::NoStorageMapEntry(pallet.into(), item.into()))
+            }
+            other => other,
+        }
     }
 }
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, PartialEq)]
 pub enum ApiError {
-    NoStorage(String, String),
+    StorageAccessFailure,
+    NoStorage,
+    NoStorageMapEntry(String, String),
+    NoStorageValue(String, String),
     DecodeError(DecodeError),
 }
 
 impl Display for ApiError {
     fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), std::fmt::Error> {
         match self {
-            ApiError::NoStorage(pallet, item) => write!(f, "no storage under {}.{}", pallet, item),
+            ApiError::StorageAccessFailure => {
+                write!(f, "blockchain error during a storage read attempt")
+            }
+            ApiError::NoStorage => write!(f, "no storage found"),
+            ApiError::NoStorageMapEntry(pallet, item) => {
+                write!(f, "storage map element not found under {}{}", pallet, item)
+            }
+            ApiError::NoStorageValue(pallet, item) => {
+                write!(f, "storage value not found under {}{}", pallet, item)
+            }
             ApiError::DecodeError(error) => write!(f, "decode error: {:?}", error),
         }
     }
@@ -115,10 +154,190 @@ where
             return Ok(authorities);
         }
 
-        let queued_keys: QueuedKeys = self.read_storage("Session", "QueuedKeys", at)?;
+        let queued_keys: QueuedKeys = self.read_storage_value("Session", "QueuedKeys", at)?;
         Ok(queued_keys
             .into_iter()
             .filter_map(|(account_id, keys)| keys.get(AURA).map(|key| (account_id, key)))
             .collect())
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use std::{
+        collections::{BTreeMap, HashMap},
+        sync::Arc,
+    };
+
+    use frame_support::Twox64Concat;
+    use parity_scale_codec::Encode;
+    use primitives::Hash;
+    use sp_runtime::Storage;
+    use substrate_test_client::ClientExt;
+
+    use super::*;
+    use crate::testing::mocks::{TestClientBuilder, TestClientBuilderExt};
+
+    #[tokio::test]
+    async fn test_proper_storage_reads() {
+        let pallet = twox_128("Pallet".as_bytes());
+        let map = twox_128("Map".as_bytes());
+        let key1 = Twox64Concat::hash("Key1".encode().as_slice());
+        let key2 = Twox64Concat::hash("Key2".encode().as_slice());
+
+        let mut map_path1 = [pallet, map].concat();
+        map_path1.extend(key1);
+        let mut map_path2 = [pallet, map].concat();
+        map_path2.extend(key2);
+
+        let storage_value = twox_128("StorageValue".as_bytes());
+        let storage_value_path = [pallet, storage_value].concat();
+
+        let storage = Storage {
+            top: BTreeMap::from([
+                (map_path1, 1u32.encode()),
+                (map_path2, 2u32.encode()),
+                (storage_value_path, 3u32.encode()),
+            ]),
+            children_default: HashMap::new(),
+        };
+
+        let mut client_builder = TestClientBuilder::new();
+        *client_builder.genesis_init_mut().extra_storage() = storage;
+        let client = Arc::new(client_builder.build());
+        let genesis_hash = client.genesis_hash();
+        let runtime_api = RuntimeApiImpl::new(client);
+
+        let map_value1 = runtime_api.read_storage_map::<Twox64Concat, u32, &str>(
+            "Pallet",
+            "Map",
+            "Key1",
+            genesis_hash,
+        );
+        let map_value2 = runtime_api.read_storage_map::<Twox64Concat, u32, &str>(
+            "Pallet",
+            "Map",
+            "Key2",
+            genesis_hash,
+        );
+        let storage_value =
+            runtime_api.read_storage_value::<u32>("Pallet", "StorageValue", genesis_hash);
+
+        assert_eq!(map_value1, Ok(1));
+        assert_eq!(map_value2, Ok(2));
+        assert_eq!(storage_value, Ok(3));
+    }
+
+    #[test]
+    fn test_missing_storage() {
+        let pallet = twox_128("Pallet".as_bytes());
+        let map = twox_128("Map".as_bytes());
+        let key1 = Twox64Concat::hash("Key1".encode().as_slice());
+        let mut map_path1 = [pallet, map].concat();
+        map_path1.extend(key1);
+
+        let storage = Storage {
+            top: BTreeMap::from([(map_path1, 1u32.encode())]),
+            children_default: HashMap::new(),
+        };
+
+        let mut client_builder = TestClientBuilder::new();
+        *client_builder.genesis_init_mut().extra_storage() = storage;
+        let client = Arc::new(client_builder.build());
+        let genesis_hash = client.genesis_hash();
+        let runtime_api = RuntimeApiImpl::new(client);
+
+        let result1 = runtime_api.read_storage_map::<Twox64Concat, u32, &str>(
+            "Pallet",
+            "Map",
+            "Key2", // this key doesn't exist in the map
+            genesis_hash,
+        );
+        let result2 = runtime_api.read_storage_value::<u32>("Pallet", "StorageValue", genesis_hash);
+
+        assert_eq!(
+            result1,
+            Err(ApiError::NoStorageMapEntry("Pallet".into(), "Map".into()))
+        );
+        assert_eq!(
+            result2,
+            Err(ApiError::NoStorageValue(
+                "Pallet".into(),
+                "StorageValue".into()
+            ))
+        );
+    }
+
+    #[test]
+    fn test_wrong_data_type_decode_error() {
+        let pallet = twox_128("Pallet".as_bytes());
+        let map = twox_128("Map".as_bytes());
+        let key1 = Twox64Concat::hash("Key1".encode().as_slice());
+        let mut map_path1 = [pallet, map].concat();
+        map_path1.extend(key1);
+        let storage_value = twox_128("StorageValue".as_bytes());
+        let storage_value_path = [pallet, storage_value].concat();
+
+        let storage = Storage {
+            top: BTreeMap::from([
+                (map_path1, 1u32.encode()),
+                (storage_value_path, 3u32.encode()),
+            ]),
+            children_default: HashMap::new(),
+        };
+
+        let mut client_builder = TestClientBuilder::new();
+        *client_builder.genesis_init_mut().extra_storage() = storage;
+        let client = Arc::new(client_builder.build());
+        let genesis_hash = client.genesis_hash();
+        let runtime_api = RuntimeApiImpl::new(client);
+
+        // parameterize function with String instead of u32
+        let result1 = runtime_api.read_storage_map::<Twox64Concat, String, &str>(
+            "Pallet",
+            "Map",
+            "Key1",
+            genesis_hash,
+        );
+        let result2 =
+            runtime_api.read_storage_value::<String>("Pallet", "StorageValue", genesis_hash);
+
+        assert!(matches!(result1, Err(ApiError::DecodeError(_))));
+        assert!(matches!(result2, Err(ApiError::DecodeError(_))));
+    }
+
+    #[test]
+    fn test_access_at_nonexistent_block() {
+        let pallet = twox_128("Pallet".as_bytes());
+        let map = twox_128("Map".as_bytes());
+        let key1 = Twox64Concat::hash("Key1".encode().as_slice());
+        let mut map_path1 = [pallet, map].concat();
+        map_path1.extend(key1);
+        let storage_value = twox_128("StorageValue".as_bytes());
+        let storage_value_path = [pallet, storage_value].concat();
+
+        let storage = Storage {
+            top: BTreeMap::from([
+                (map_path1, 1u32.encode()),
+                (storage_value_path, 3u32.encode()),
+            ]),
+            children_default: HashMap::new(),
+        };
+
+        let mut client_builder = TestClientBuilder::new();
+        *client_builder.genesis_init_mut().extra_storage() = storage;
+        let client = Arc::new(client_builder.build());
+        let runtime_api = RuntimeApiImpl::new(client);
+
+        let result1 = runtime_api.read_storage_map::<Twox64Concat, u32, &str>(
+            "Pallet",
+            "Map",
+            "Key1",
+            Hash::zero(),
+        );
+        let result2 = runtime_api.read_storage_value::<u32>("Pallet", "StorageValue", Hash::zero());
+
+        assert_eq!(result1, Err(ApiError::StorageAccessFailure));
+        assert_eq!(result2, Err(ApiError::StorageAccessFailure));
     }
 }
