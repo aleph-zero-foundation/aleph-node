30b0e178107500a1bbfe92d9ede3bfd3906a781b
Piotr Mikołajczyk <piomiko41@gmail.com>
2024-02-20 11:12:25 +0100

    A0-4036: Fix nightly tests (#1625)
    # Description
    
    Testing chain extension requires dedicated toolchain - copied steps from
    the PR flow.
    
    
    https://github.com/Cardinal-Cryptography/aleph-node/actions/runs/7970662104
    
    ## Type of change
    
    - Bug fix (non-breaking change which fixes an issue)

--- baby-liminal-extension/src/backend/tests.rs
+++ baby-liminal-extension/src/backend/tests.rs
@@ -6,7 +6,7 @@ mod weighting;
 use aleph_runtime::Runtime as AlephRuntime;
 use aleph_runtime_interfaces::snark_verifier::VerifierError::*;
 use frame_support::pallet_prelude::Weight;
-use pallet_contracts::chain_extension::{ChainExtension, RetVal};
+use pallet_contracts::chain_extension::RetVal;
 
 use crate::{
     backend::{
