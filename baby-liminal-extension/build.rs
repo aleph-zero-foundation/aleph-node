aleph-zero-foundation/aleph-node

commit aa6826960a001dbbaf947a65c42ea8fc08b5dc83

A0-4036: Incorporate on-chain verifier to the node (remove `liminal` feature) (#1614)
# Description

1. include `pallet-vk-storage`, `baby-liminal-extension`,
`runtime-interfaces` in the Rust workspace
2. clean-up CI (remove liminal-specific jobs and workflows)
3. remove `liminal` feature from all crates (inluding `aleph-runtime`,
`aleph-node`, `aleph-client`, `runtime-interfaces`, scripts)

Direct follow-up: _put on-chain verifier behind runtime-checks using
`feature-control` pallet_ - I didn't include that here so that this PR
is purely logistic, without additional runtime logic to check.

Note: I removed `clippy` check for the `ink(-std)` feature for
`baby-liminal-extension`, because:
- the crate has `aleph-runtime` in its `dev-dependencies` - it is used
for testing `runtime-std` feature (mutually exclusive with `ink-std`)
- `aleph-runtime` depends on `baby-liminal-extension` with the `runtime`
feature
- and thus `baby-liminal-extension` is eventually built with both
`ink-std` and `runtime` features, which leads to compilation failure


## Type of change

- New feature (non-breaking change which adds functionality)
- Breaking change (fix or feature that would cause existing
functionality to not work as expected)

# Checklist:

- I have made neccessary updates to the Infrastructure
- I have made corresponding changes to the existing documentation

bin/runtime/Cargo.toml
@@ -65,10 +65,9 @@ pallet-elections = { workspace = true }
 pallet-feature-control = { workspace = true }
 primitives = { workspace = true }
 pallet-proxy = { workspace = true }
+pallet-vk-storage = { workspace = true }
 
-# Liminal-related dependencies
-baby-liminal-extension = { workspace = true, features = ["runtime"], optional = true }
-pallet-vk-storage = { workspace = true, optional = true }
+baby-liminal-extension = { workspace = true, features = ["runtime"] }
 
 [build-dependencies]
 substrate-wasm-builder = { workspace = true }
@@ -88,6 +87,7 @@ std = [
     "pallet-authorship/std",
     "pallet-balances/std",
     "pallet-elections/std",
+    "pallet-feature-control/std",
     "pallet-identity/std",
     "pallet-insecure-randomness-collective-flip/std",
     "pallet-session/std",
@@ -101,6 +101,7 @@ std = [
     "pallet-multisig/std",
     "pallet-proxy/std",
     "pallet-utility/std",
+    "pallet-vk-storage/std",
     "sp-api/std",
     "sp-block-builder/std",
     "sp-consensus-aura/std",
@@ -124,6 +125,7 @@ std = [
     "sp-io/std",
     "scale-info/std",
     "sp-application-crypto/std",
+    "baby-liminal-extension/runtime-std",
 ]
 short_session = ["primitives/short_session"]
 try-runtime = [
@@ -153,33 +155,12 @@ try-runtime = [
     "pallet-committee-management/try-runtime",
     "pallet-proxy/try-runtime",
     "pallet-feature-control/try-runtime",
+    "pallet-vk-storage/try-runtime",
 ]
 enable_treasury_proposals = []
 runtime-benchmarks = [
     "frame-system/runtime-benchmarks",
     "frame-benchmarking/runtime-benchmarks",
     "pallet-feature-control/runtime-benchmarks",
-]
-
-# Liminal-related features
-liminal = [
-    "baby-liminal-extension",
-    "pallet-vk-storage",
-]
-liminal-std = [
-    "liminal",
-    "std",
-    "baby-liminal-extension/runtime-std",
-    "pallet-vk-storage/std",
-]
-liminal-try-runtime = [
-    "liminal",
-    "try-runtime",
-    "pallet-vk-storage/try-runtime",
-]
-liminal-runtime-benchmarks = [
-    "liminal",
-    "runtime-benchmarks",
-    "baby-liminal-extension/runtime-benchmarks",
     "pallet-vk-storage/runtime-benchmarks",
 ]

Cargo.toml
@@ -2,6 +2,7 @@
 
 members = [
     "aggregator",
+    "baby-liminal-extension",
     "bin/node",
     "bin/runtime",
     "clique",
@@ -11,21 +12,20 @@ members = [
     "pallets/feature-control",
     "pallets/committee-management",
     "pallets/support",
+    "pallets/vk-storage",
     "primitives",
     "rate-limiter",
+    "runtime-interfaces",
 ]
 
 exclude = [
     "aleph-client",
-    "baby-liminal-extension",
     "benches/payout-stakers",
     "bin/cliain",
     "bin/finalizer",
     "contracts/adder",
     "e2e-tests",
     "flooder",
-    "pallets/vk-storage",
-    "runtime-interfaces",
     "scripts/synthetic-network/synthetic-link",
 ]
 
@@ -45,6 +45,7 @@ homepage = "https://alephzero.org"
 repository = "https://github.com/Cardinal-Cryptography/aleph-node"
 
 [workspace.dependencies]
+ahash = { version = "=0.8.6", default-features = false } # reqiured until Rust is updated to 1.71 (https://solana.stackexchange.com/questions/8800/error-use-of-unstable-library-feature-build-hasher-simple-hash-one)
 aleph-bft-crypto = { version = "0.8" }
 aleph-bft-mock = { version = "0.11.1" }
 aleph-bft-rmc = { version = "0.11" }
@@ -58,6 +59,7 @@ futures-timer = { version = "3.0" }
 hash-db = { version = "0.16", default-features = false }
 hex = { version = "0.4" }
 hex-literal = { version = "0.3" }
+ink = { version = "5.0.0-rc" }
 ip_network = { version = "0.4" }
 jsonrpsee = { version = "0.16.3" }
 libp2p = { version = "0.51" }
@@ -164,7 +166,13 @@ substrate-wasm-builder = { git = "https://github.com/Cardinal-Cryptography/polka
 
 try-runtime-cli = { git = "https://github.com/Cardinal-Cryptography/polkadot-sdk.git", branch = "aleph-v1.2.0" }
 
+halo2_proofs = { git = "https://github.com/Cardinal-Cryptography/pse-halo2", branch = "aleph", default-features = false }
+
+# Local crates
+
 aleph-runtime = { path = "bin/runtime" }
+aleph-runtime-interfaces = { path = "runtime-interfaces", default-features = false }
+baby-liminal-extension = { path = "baby-liminal-extension", default-features = false }
 finality-aleph = { path = "finality-aleph" }
 network-clique = { path = "clique" }
 rate-limiter = { path = "rate-limiter" }
@@ -173,10 +181,5 @@ pallet-committee-management = { path = "pallets/committee-management", default-f
 pallet-elections = { path = "pallets/elections", default-features = false }
 pallet-feature-control = { path = "pallets/feature-control", default-features = false }
 pallets-support = { path = "pallets/support", default-features = false }
-primitives = { path = "primitives", default-features = false }
-
-# Liminal-related dependencies
-
-aleph-runtime-interfaces = { path = "runtime-interfaces", default-features = false }
-baby-liminal-extension = { path = "baby-liminal-extension", default-features = false }
 pallet-vk-storage = { path = "pallets/vk-storage", default-features = false }
+primitives = { path = "primitives", default-features = false }

aleph-client/Cargo.toml
@@ -30,6 +30,3 @@ primitives = { path = "../primitives" }
 
 [dev-dependencies]
 tokio = "1.21"
-
-[features]
-liminal = []

baby-liminal-extension/Cargo.toml
@@ -1,45 +1,47 @@
 [package]
 name = "baby-liminal-extension"
 version = "0.1.0"
-authors = ["Cardinal"]
-edition = "2021"
-homepage = "https://alephzero.org"
-repository = "https://github.com/aleph-zero-foundation/aleph-node"
+authors.workspace = true
+edition.workspace = true
+homepage.workspace = true
+repository.workspace = true
 license = "Apache-2.0"
 
 [dependencies]
 # Frontend dependencies:
 
-ink = { version = "5.0.0-rc", default-features = false, optional = true }
-sp-core = { default-features = false, git = "https://github.com/Cardinal-Cryptography/polkadot-sdk.git", branch = "aleph-v1.2.0", optional = true }
+ink = { workspace = true, optional = true }
+sp-core = { workspace = true, optional = true }
 
 # Backend dependencies:
 
-log = { version = "0.4", default-features = false, optional = true }
-scale = { package = "parity-scale-codec", version = "3", default-features = false, features = ["derive"], optional = true }
+ahash = { workspace = true, optional = true } # Required until Rust is updated to 1.71
+getrandom = { version = "0.2.12", features = ["js"], optional = true }
+log = { workspace = true, optional = true }
+parity-scale-codec = { workspace = true, features = ["derive"], optional = true }
 
-frame-support = { git = "https://github.com/Cardinal-Cryptography/polkadot-sdk.git", branch = "aleph-v1.2.0", default-features = false, optional = true }
-frame-system = { git = "https://github.com/Cardinal-Cryptography/polkadot-sdk.git", branch = "aleph-v1.2.0", default-features = false, optional = true }
-pallet-contracts = { git = "https://github.com/Cardinal-Cryptography/polkadot-sdk.git", branch = "aleph-v1.2.0", default-features = false, optional = true }
-sp-std = { git = "https://github.com/Cardinal-Cryptography/polkadot-sdk.git", branch = "aleph-v1.2.0", default-features = false, optional = true }
+frame-support = { workspace = true, optional = true }
+frame-system = { workspace = true, optional = true }
+pallet-contracts = { workspace = true, optional = true }
+sp-std = { workspace = true, optional = true }
 
 pallet-feature-control = { path = "../pallets/feature-control", default-features = false, optional = true }
 
 ## Proof verification dependencies:
 
-aleph-runtime-interfaces = { path = "../runtime-interfaces", default-features = false, optional = true }
-pallet-vk-storage = { path = "../pallets/vk-storage", default-features = false, optional = true }
+aleph-runtime-interfaces = { workspace = true, optional = true }
+pallet-vk-storage = { workspace = true, optional = true }
 
 # Benchmarking
 
-frame-benchmarking = { default-features = false, git = "https://github.com/Cardinal-Cryptography/polkadot-sdk.git", branch = "aleph-v1.2.0", optional = true }
-paste = { version = "1.0.11", optional = true }
+frame-benchmarking = { workspace = true, optional = true }
+paste = { workspace = true, optional = true }
 
 [dev-dependencies]
-aleph-runtime = { path = "../bin/runtime", features = ["liminal"] }
+aleph-runtime = { workspace = true }
 
 [build-dependencies]
-halo2_proofs = { git = "https://github.com/Cardinal-Cryptography/pse-halo2", branch = "aleph", default-features = false, features = ["mock-kzg-params", "multicore"] }
+halo2_proofs = { workspace = true, features = ["mock-kzg-params", "multicore"] }
 
 [features]
 # This is an artificial feature that is used by 3rd party crates in macro expansions.
@@ -47,27 +49,29 @@ std = []
 
 # `runtime` and `runtime-std` features are dedicated to the runtime crate. They bring the backend part of the extension.
 runtime = [
+    "getrandom",
+    "ahash",
     "log",
-    "scale",
+    "parity-scale-codec",
     "frame-support",
     "frame-system",
     "pallet-contracts",
     "sp-std",
     "pallet-feature-control",
     "pallet-vk-storage",
-    "aleph-runtime-interfaces/liminal",
+    "aleph-runtime-interfaces",
 ]
 runtime-std = [
     "runtime",
     "std",
-    "scale/std",
+    "parity-scale-codec/std",
     "frame-support/std",
     "frame-system/std",
     "pallet-contracts/std",
     "sp-std/std",
     "pallet-feature-control/std",
     "pallet-vk-storage/std",
-    "aleph-runtime-interfaces/liminal-std",
+    "aleph-runtime-interfaces/std",
 ]
 
 # `ink` and `ink-std` features are dedicated to the ink! part of the extension. They bring the smart contract part of

baby-liminal-extension/test_contract/Cargo.toml
@@ -1,3 +1,5 @@
+[workspace]
+
 [package]
 name = "baby-liminal-extension-test-contract"
 version = "0.1.0"

bin/cliain/Cargo.toml
@@ -29,6 +29,3 @@ primitives = { path = "../../primitives" }
 [features]
 default = ["std"]
 std = []
-liminal = [
-    "aleph_client/liminal",
-]

bin/node/Cargo.toml
@@ -63,7 +63,7 @@ frame-benchmarking-cli = { workspace = true, optional = true }
 frame-benchmarking = { workspace = true, optional = true }
 
 aleph-runtime = { workspace = true }
-aleph-runtime-interfaces = { workspace = true, optional = true }
+aleph-runtime-interfaces = { workspace = true }
 finality-aleph = { workspace = true }
 primitives = { workspace = true }
 
@@ -100,20 +100,4 @@ runtime-benchmarks = [
 ]
 only_legacy = [
     "finality-aleph/only_legacy"
-]
-
-# Liminal-related features
-liminal = [
-    "aleph-runtime/liminal",
-    "aleph-runtime-interfaces/liminal-std",
-]
-liminal-try-runtime = [
-    "liminal",
-    "try-runtime",
-    "aleph-runtime/liminal-try-runtime",
-]
-liminal-runtime-benchmarks = [
-    "liminal",
-    "runtime-benchmarks",
-    "aleph-runtime/liminal-runtime-benchmarks"
 ]

pallets/vk-storage/Cargo.toml
@@ -1,32 +1,34 @@
 [package]
 name = "pallet-vk-storage"
 version = "0.1.0"
-authors = ["Cardinal Cryptography"]
-edition = "2021"
+authors.workspace = true
+edition.workspace = true
+homepage.workspace = true
+repository.workspace = true
 license = "Apache 2.0"
 
 [dependencies]
-log = { version = "0.4", default-features = false }
-codec = { package = "parity-scale-codec", version = "3.0", default-features = false, features = ["derive"] }
-scale-info = { version = "2.0", default-features = false, features = ["derive"] }
+log = { workspace = true }
+parity-scale-codec = { workspace = true, features = ["derive"] }
+scale-info = { workspace = true, features = ["derive"] }
 
-frame-benchmarking = { default-features = false, git = "https://github.com/Cardinal-Cryptography/polkadot-sdk.git", branch = "aleph-v1.2.0", optional = true }
-frame-support = { default-features = false, git = "https://github.com/Cardinal-Cryptography/polkadot-sdk.git", branch = "aleph-v1.2.0" }
-frame-system = { default-features = false, git = "https://github.com/Cardinal-Cryptography/polkadot-sdk.git", branch = "aleph-v1.2.0" }
-sp-core = { default-features = false, git = "https://github.com/Cardinal-Cryptography/polkadot-sdk.git", branch = "aleph-v1.2.0" }
-sp-std = { default-features = false, git = "https://github.com/Cardinal-Cryptography/polkadot-sdk.git", branch = "aleph-v1.2.0" }
+frame-benchmarking = { workspace = true, optional = true }
+frame-support = { workspace = true }
+frame-system = { workspace = true }
+sp-core = { workspace = true }
+sp-std = { workspace = true }
 
 [dev-dependencies]
-pallet-balances = { default-features = false, git = "https://github.com/Cardinal-Cryptography/polkadot-sdk.git", branch = "aleph-v1.2.0" }
-sp-io = { default-features = false, git = "https://github.com/Cardinal-Cryptography/polkadot-sdk.git", branch = "aleph-v1.2.0" }
-sp-runtime = { default-features = false, git = "https://github.com/Cardinal-Cryptography/polkadot-sdk.git", branch = "aleph-v1.2.0" }
+pallet-balances = { workspace = true }
+sp-io = { workspace = true }
+sp-runtime = { workspace = true }
 
 [features]
 default = ["std"]
 std = [
     "log/std",
 
-    "codec/std",
+    "parity-scale-codec/std",
 
     "frame-benchmarking/std",
     "frame-system/std",

commit d79d6ac0b3cfb115cf73c9051d0e02f675dcfaaf

A0-4209: Extension build script (#1664)
# Description

When building `baby-liminal-extension` with `runtime-benchmarks` feature
on, an extremely heavy computation is started (takes like 2h); however,
we already have all the (deterministic) artifacts kept under VCS and
thus, we shouldn’t recompute them if they are present.

Also, we fix a regression in feature selection for the `aleph-runtime`
crate (introduced in #1614)

## Type of change

- Bug fix (non-breaking change which fixes an issue)

# Checklist:

- I have made corresponding changes to the existing documentation

baby-liminal-extension/build.rs
@@ -24,6 +24,9 @@
 //! and put them in the `benchmark-resources` directory. Changing the build script will trigger the artifacts generation
 //! again. On the other hand, changing any other file in this crate will not be considered as a reason for rerunning.
 //!
+//! Note: if a file for a particular circuit already exists, it will not be generated again. Furthermore, if all files
+//! are present, the trusted setup procedure (the heaviest computation) will also be skipped.
+//!
 //! # What circuits are generated?
 //!
 //! We provide a generic circuit that can be parametrized with the number of instances and the number of rows. More
@@ -40,16 +43,16 @@
 //! The rest of the gates are batches of `ROW_BLOWUP - 1` copies of the `i`th gate (`i`th batch corresponds to the `ith`
 //! gate).
 
-/// This build script is used only for the runtime benchmarking setup. We don't need to do anything here in other case.
-#[cfg(not(feature = "runtime-benchmarks"))]
-fn main() {}
-
 #[cfg(feature = "runtime-benchmarks")]
 use {
     artifacts::generate_artifacts,
     halo2_proofs::{halo2curves::bn256::Bn256, poly::kzg::commitment::ParamsKZG},
-    std::{env, fs, path::Path},
+    std::{cell::OnceCell, env, fs, path::Path},
 };
+
+/// This build script is used only for the runtime benchmarking setup. We don't need to do anything here in other case.
+#[cfg(not(feature = "runtime-benchmarks"))]
+fn main() {}
 
 #[cfg(feature = "runtime-benchmarks")]
 fn main() {
@@ -61,7 +64,7 @@ fn main() {
     // developer convenience.
     const CIRCUIT_MAX_K: u32 = 20;
     // We run a common setup for all generated circuits.
-    let params = ParamsKZG::<Bn256>::setup(CIRCUIT_MAX_K, ParamsKZG::<Bn256>::mock_rng());
+    let params = OnceCell::new();
 
     let path = |instances, row_blowup, suf| {
         Path::new(&env::current_dir().unwrap())
@@ -74,9 +77,22 @@ fn main() {
 
     for instances in INSTANCES {
         for row_blowup in ROW_BLOWUP {
-            let artifacts = generate_artifacts(*instances, *row_blowup, &params);
-
             let path = |suf| path(*instances, *row_blowup, suf);
+            if [path("vk"), path("proof"), path("input")]
+                .into_iter()
+                .all(|p| p.exists())
+            {
+                continue;
+            }
+
+            let artifacts = generate_artifacts(
+                *instances,
+                *row_blowup,
+                params.get_or_init(|| {
+                    ParamsKZG::<Bn256>::setup(CIRCUIT_MAX_K, ParamsKZG::<Bn256>::mock_rng())
+                }),
+            );
+
             fs::write(&path("vk"), artifacts.verification_key).unwrap();
             fs::write(&path("proof"), artifacts.proof).unwrap();
             fs::write(&path("input"), artifacts.public_input).unwrap();

bin/runtime/Cargo.toml
@@ -61,7 +61,7 @@ sp-application-crypto = { workspace = true }
 frame-benchmarking = { workspace = true, optional = true }
 
 pallet-aleph = { workspace = true }
-pallet-committee-management= { workspace = true }
+pallet-committee-management = { workspace = true }
 pallet-elections = { workspace = true }
 pallet-feature-control = { workspace = true }
 pallet-operations = { workspace = true }
@@ -166,6 +166,7 @@ enable_treasury_proposals = []
 runtime-benchmarks = [
     "frame-system/runtime-benchmarks",
     "frame-benchmarking/runtime-benchmarks",
+    "baby-liminal-extension/runtime-benchmarks",
     "pallet-feature-control/runtime-benchmarks",
     "pallet-vk-storage/runtime-benchmarks",
 ]