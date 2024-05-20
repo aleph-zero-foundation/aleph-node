fbdf102e3e0621467a6a6550991dfb3eeb447c68
Marcin <marcin.radecki@cardinals.cc>
2024-05-20 11:56:05 +0000

    A0-4268: chain-bootstrapper CI (#1720)
    # Description
    
    This PR contains changes required for feature nets to work from `main`
    again. It also adds proper CI support for `chain-bootstrapper`.
    Workflows that build `aleph-node` were merged into one to avoid code
    duplication. The `key` subcommand was added to `chain-bootstrapper`—it
    does not increase compilation time and is used by the FE engine to
    bootstrap the chain. Also, there's better coupling between workflows as
    they output GH Artifact names that other workflows consume.
    
    ## Type of change
    
    Please delete options that are not relevant.
    
    - Bug fix (non-breaking change which fixes an issue)
    - New feature (non-breaking change which adds functionality)
    
    ## Testing
    
    Update net tests are omitted since they needs to be fixed on `main`
    anyway.
    
    1. [FE create from
    `main`](https://github.com/Cardinal-Cryptography/aleph-node/actions/runs/9090690639/job/24983952063)
    - OK
    2. [FE created from
    Mainnet](https://github.com/Cardinal-Cryptography/aleph-node/actions/runs/9091207050/job/24985384641)
    - OK
    3. [On push to main workflow
    test](https://github.com/Cardinal-Cryptography/aleph-node/actions/runs/9091734325)
    - OK
    4. [Nightly pipeline normal session e2e
    tests](https://github.com/Cardinal-Cryptography/aleph-node/actions/runs/9092279244)
    - OK
    5. [Nightly pipeline e2e tests on
    featurenet](https://github.com/Cardinal-Cryptography/aleph-node/actions/runs/9092281415)
    - OK
    
    ## Actions before merge PR
    Replace all `A0-4268` and `v6` to `v7`
    
    ## Companion PRs
    https://github.com/Cardinal-Cryptography/featurenet-template/pull/30
    https://github.com/Cardinal-Cryptography/github-actions/pull/49

--- bin/chain-bootstrapper/src/main.rs
+++ bin/chain-bootstrapper/src/main.rs
@@ -1,6 +1,10 @@
 mod chain_spec;
 
-use sc_cli::clap::{self, Parser, Subcommand as ClapSubcommand};
+use sc_chain_spec::ChainSpec;
+use sc_cli::{
+    clap::{self, Parser, Subcommand as ClapSubcommand},
+    SubstrateCli,
+};
 
 use crate::chain_spec::{BootstrapChainCmd, ConvertChainspecToRawCmd};
 
@@ -8,6 +12,36 @@ use crate::chain_spec::{BootstrapChainCmd, ConvertChainspecToRawCmd};
 struct Cli {
     #[command(subcommand)]
     pub subcommand: Option<Subcommand>,
+}
+
+impl SubstrateCli for Cli {
+    fn impl_name() -> String {
+        panic!("This is not used.")
+    }
+
+    fn impl_version() -> String {
+        panic!("This is not used.")
+    }
+
+    fn description() -> String {
+        panic!("This is not used.")
+    }
+
+    fn author() -> String {
+        panic!("This is not used.")
+    }
+
+    fn support_url() -> String {
+        panic!("This is not used.")
+    }
+
+    fn copyright_start_year() -> i32 {
+        panic!("This is not used.")
+    }
+
+    fn load_spec(&self, _id: &str) -> Result<Box<dyn ChainSpec>, String> {
+        panic!("This is not used.")
+    }
 }
 
 #[allow(clippy::large_enum_variant)]
@@ -18,6 +52,10 @@ pub enum Subcommand {
 
     /// Takes a chainspec and generates a corresponding raw chainspec
     ConvertChainspecToRaw(ConvertChainspecToRawCmd),
+
+    /// Key management cli utilities
+    #[command(subcommand)]
+    Key(sc_cli::KeySubcommand),
 }
 
 fn main() -> sc_cli::Result<()> {
@@ -26,6 +64,7 @@ fn main() -> sc_cli::Result<()> {
     match &cli.subcommand {
         Some(Subcommand::BootstrapChain(cmd)) => cmd.run(),
         Some(Subcommand::ConvertChainspecToRaw(cmd)) => cmd.run(),
+        Some(Subcommand::Key(cmd)) => cmd.run(&cli),
 
         None => Err("Command was required!".into()),
     }
