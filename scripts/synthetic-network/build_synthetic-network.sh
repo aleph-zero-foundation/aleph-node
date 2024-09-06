479bdf0c0f85911edde469a69ccdf8574894e113
fixxxedpoint <lukasz.lachowski@cardinals.cc>
2024-09-06 10:50:53 +0200

    L1-278: Fix synthetic-network repo compilation (#1807)
    # Description
    
    Added a custom version of the Dockerfile from the repo of the
    synthetic-network tool that uses a more specific version of the rust
    compiler. Also removed an ad-hoc fix for node-js images used by the
    original Dockerfile.
    
    ## Type of change
    
    - Bug fix (non-breaking change which fixes an issue)

--- scripts/synthetic-network/build_synthetic-network.sh
+++ scripts/synthetic-network/build_synthetic-network.sh
@@ -11,24 +11,10 @@ if [[ "$UPDATE" = true ]]; then
     git submodule update --remote
 fi
 
-pushd .
-cd scripts/synthetic-network/vendor/synthetic-network
-
-# this is a dirty-fix for the outdated version of node image used by the
-# synthetic-network's Dockerfile
-# TODO remove it after this push-request is merged into synthetic-network: https://github.com/daily-co/synthetic-network/pull/14
-docker pull node:20.3.0
-docker tag node:20.3.0 node:12.20.2
-
-log "building base docker image for synthetic-network with support for synthetic-network"
-docker build --tag syntheticnet --file Dockerfile .
-
-popd
+log "building docker image for synthetic-network"
+docker build --tag syntheticnet --file docker/Dockerfile.synthetic_network.build scripts/synthetic-network/vendor/synthetic-network
 
 log "building docker image for aleph-node that supports synthetic-network"
-docker build -t aleph-node:syntheticnet -f docker/Dockerfile.synthetic_network .
-
-# clean previously tagged image
-docker image rm node:12.20.2
+docker build --tag aleph-node:syntheticnet --file docker/Dockerfile.synthetic_network .
 
 exit 0
