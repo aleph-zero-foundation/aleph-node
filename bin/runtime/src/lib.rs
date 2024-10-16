98091cd2d64d312db08a491f71b3a7bf321d2f5c
kostekIV <27210860+kostekIV@users.noreply.github.com>
2024-10-16 17:21:26 +0100

    L1-305: Add migrations (#1836)

--- bin/runtime/src/lib.rs
+++ bin/runtime/src/lib.rs
@@ -1033,6 +1033,13 @@ pub type SignedBlock = generic::SignedBlock<Block>;
 /// BlockId type as expected by this runtime.
 pub type BlockId = generic::BlockId<Block>;
 
+pub type Migrations = (
+    pallet_nomination_pools::migration::versioned::V5toV6<Runtime>,
+    pallet_nomination_pools::migration::versioned::V6ToV8<Runtime>,
+    pallet_staking::migrations::v14::MigrateToV14<Runtime>,
+    pallet_identity::migration::versioned::V0ToV1<Runtime, { u64::MAX }>,
+);
+
 /// Executive: handles dispatch to the various modules.
 pub type Executive = frame_executive::Executive<
     Runtime,
@@ -1040,6 +1047,7 @@ pub type Executive = frame_executive::Executive<
     frame_system::ChainContext<Runtime>,
     Runtime,
     AllPalletsWithSystem,
+    Migrations,
 >;
 
 #[cfg(feature = "runtime-benchmarks")]
