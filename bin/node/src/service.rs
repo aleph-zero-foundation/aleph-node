aleph-zero-foundation/aleph-node

commit 1d65f62de7e090d07b0e6f2b2261c3edaa9583a1

A0-2674: Refactor service.rs (#1623)
part 2/x of refactoring

bin/node/src/service.rs
@@ -12,24 +12,23 @@ use finality_aleph::{
     ProtocolNaming, RateLimiterConfig, RedirectingBlockImport, SessionPeriod, SubstrateChainStatus,
     SubstrateNetwork, SyncOracle, TracingBlockImport, ValidatorAddressCache,
 };
-use futures::channel::mpsc;
 use log::warn;
+use sc_basic_authorship::ProposerFactory;
 use sc_client_api::{BlockBackend, HeaderBackend};
 use sc_consensus::ImportQueue;
 use sc_consensus_aura::{ImportQueueParams, SlotProportion, StartAuraParams};
 use sc_consensus_slots::BackoffAuthoringBlocksStrategy;
-use sc_service::{
-    error::Error as ServiceError, Configuration, KeystoreContainer, NetworkStarter, RpcHandlers,
-    TFullClient, TaskManager,
-};
+use sc_network::config::FullNetworkConfiguration;
+use sc_service::{error::Error as ServiceError, Configuration, TFullClient, TaskManager};
 use sc_telemetry::{Telemetry, TelemetryWorker};
 use sp_api::ProvideRuntimeApi;
 use sp_arithmetic::traits::BaseArithmetic;
+use sp_consensus::DisableProofRecording;
 use sp_consensus_aura::{sr25519::AuthorityPair as AuraPair, Slot};
 
 use crate::{
     aleph_cli::AlephCli,
-    aleph_primitives::{AlephSessionApi, BlockHash, MAX_BLOCK_SIZE},
+    aleph_primitives::{AlephSessionApi, MAX_BLOCK_SIZE},
     chain_spec::DEFAULT_BACKUP_FOLDER,
     executor::AlephExecutor,
     rpc::{create_full as create_full_rpc, FullDeps as RpcFullDeps},
@@ -38,12 +37,16 @@ use crate::{
 type FullClient = sc_service::TFullClient<Block, RuntimeApi, AlephExecutor>;
 type FullBackend = sc_service::TFullBackend<Block>;
 type FullSelectChain = sc_consensus::LongestChain<FullBackend, Block>;
+type FullPool = sc_transaction_pool::FullPool<Block, FullClient>;
+type FullImportQueue = sc_consensus::DefaultImportQueue<Block>;
+type FullProposerFactory =
+    ProposerFactory<FullPool, FullBackend, FullClient, DisableProofRecording>;
 type ServiceComponents = sc_service::PartialComponents<
     FullClient,
     FullBackend,
     FullSelectChain,
-    sc_consensus::DefaultImportQueue<Block>,
-    sc_transaction_pool::FullPool<Block, FullClient>,
+    FullImportQueue,
+    FullPool,
     (
         ChannelProvider<Justification>,
         Option<Telemetry>,
@@ -183,30 +186,44 @@ pub fn new_partial(config: &Configuration) -> Result<ServiceComponents, ServiceE
     })
 }
 
-#[allow(clippy::type_complexity)]
-#[allow(clippy::too_many_arguments)]
-fn setup(
-    config: Configuration,
-    backend: Arc<FullBackend>,
-    chain_status: SubstrateChainStatus,
-    keystore_container: &KeystoreContainer,
-    import_queue: sc_consensus::DefaultImportQueue<Block>,
-    transaction_pool: Arc<sc_transaction_pool::FullPool<Block, FullClient>>,
-    task_manager: &mut TaskManager,
-    client: Arc<FullClient>,
-    telemetry: &mut Option<Telemetry>,
-    import_justification_tx: mpsc::UnboundedSender<Justification>,
-    collect_extra_debugging_data: bool,
-) -> Result<
-    (
-        RpcHandlers,
-        SubstrateNetwork<Block, BlockHash>,
-        NetworkStarter,
-        SyncOracle,
-        Option<ValidatorAddressCache>,
-    ),
-    ServiceError,
-> {
+struct AlephRuntimeVars {
+    pub session_period: SessionPeriod,
+    pub millisecs_per_block: MillisecsPerBlock,
+}
+
+fn get_aleph_runtime_vars(client: &Arc<FullClient>) -> AlephRuntimeVars {
+    let finalized = client.info().finalized_hash;
+
+    let session_period = SessionPeriod(
+        client
+            .runtime_api()
+            .session_period(finalized)
+            .expect("should always be available"),
+    );
+
+    let millisecs_per_block = MillisecsPerBlock(
+        client
+            .runtime_api()
+            .millisecs_per_block(finalized)
+            .expect("should always be available"),
+    );
+
+    AlephRuntimeVars {
+        session_period,
+        millisecs_per_block,
+    }
+}
+
+fn get_validator_address_cache(aleph_config: &AlephCli) -> Option<ValidatorAddressCache> {
+    aleph_config
+        .no_collection_of_extra_debugging_data()
+        .then(ValidatorAddressCache::new)
+}
+
+fn get_net_config(
+    config: &Configuration,
+    client: &Arc<FullClient>,
+) -> (FullNetworkConfiguration, ProtocolNaming) {
     let genesis_hash = client
         .block_hash(0)
         .ok()
@@ -217,7 +234,7 @@ fn setup(
         None => format!("/{genesis_hash}"),
     };
     let protocol_naming = ProtocolNaming::new(chain_prefix);
-    let mut net_config = sc_network::config::FullNetworkConfiguration::new(&config.network);
+    let mut net_config = FullNetworkConfiguration::new(&config.network);
     net_config.add_notification_protocol(finality_aleph::peers_set_config(
         protocol_naming.clone(),
         Protocol::Authentication,
@@ -227,31 +244,109 @@ fn setup(
         Protocol::BlockSync,
     ));
 
+    (net_config, protocol_naming)
+}
+
+fn get_proposer_factory(
+    service_components: &ServiceComponents,
+    config: &Configuration,
+) -> FullProposerFactory {
+    let mut proposer_factory = FullProposerFactory::new(
+        service_components.task_manager.spawn_handle(),
+        service_components.client.clone(),
+        service_components.transaction_pool.clone(),
+        config.prometheus_registry().cloned().as_ref(),
+        None,
+    );
+    proposer_factory.set_default_block_size_limit(MAX_BLOCK_SIZE as usize);
+
+    proposer_factory
+}
+
+fn get_rate_limit_config(aleph_config: &AlephCli) -> RateLimiterConfig {
+    RateLimiterConfig {
+        alephbft_bit_rate_per_connection: aleph_config
+            .alephbft_bit_rate_per_connection()
+            .try_into()
+            .unwrap_or(usize::MAX),
+    }
+}
+
+/// Builds a new service for a full client.
+pub fn new_authority(
+    config: Configuration,
+    aleph_config: AlephCli,
+) -> Result<TaskManager, ServiceError> {
+    if aleph_config.external_addresses().is_empty() {
+        panic!("Cannot run a validator node without external addresses, stopping.");
+    }
+
+    let mut service_components = new_partial(&config)?;
+
+    let backup_path = backup_path(&aleph_config, config.base_path.path());
+
+    let backoff_authoring_blocks = Some(LimitNonfinalized(aleph_config.max_nonfinalized_blocks()));
+    let prometheus_registry = config.prometheus_registry().cloned();
+    let (sync_oracle, _) = SyncOracle::new();
+    let proposer_factory = get_proposer_factory(&service_components, &config);
+    let slot_duration = sc_consensus_aura::slot_duration(&*service_components.client)?;
+    let (block_import, block_rx) = RedirectingBlockImport::new(service_components.client.clone());
+
+    let aura = sc_consensus_aura::start_aura::<AuraPair, _, _, _, _, _, _, _, _, _, _>(
+        StartAuraParams {
+            slot_duration,
+            client: service_components.client.clone(),
+            select_chain: service_components.select_chain.clone(),
+            block_import,
+            proposer_factory,
+            create_inherent_data_providers: move |_, ()| async move {
+                let timestamp = sp_timestamp::InherentDataProvider::from_system_time();
+
+                let slot =
+                    sp_consensus_aura::inherents::InherentDataProvider::from_timestamp_and_slot_duration(
+                        *timestamp,
+                        slot_duration,
+                    );
+
+                Ok((slot, timestamp))
+            },
+            force_authoring: config.force_authoring,
+            backoff_authoring_blocks,
+            keystore: service_components.keystore_container.local_keystore(),
+            sync_oracle: sync_oracle.clone(),
+            justification_sync_link: (),
+            block_proposal_slot_portion: SlotProportion::new(2f32 / 3f32),
+            max_block_proposal_slot_portion: None,
+            telemetry: service_components.other.1.as_ref().map(|x| x.handle()),
+            compatibility_mode: Default::default(),
+        },
+    )?;
+
+    let import_queue_handle = BlockImporter::new(service_components.import_queue.service());
+    let (net_config, protocol_naming) = get_net_config(&config, &service_components.client);
     let (network, system_rpc_tx, tx_handler_controller, network_starter, sync_network) =
         sc_service::build_network(sc_service::BuildNetworkParams {
             config: &config,
             net_config,
-            client: client.clone(),
-            transaction_pool: transaction_pool.clone(),
-            spawn_handle: task_manager.spawn_handle(),
-            import_queue,
+            client: service_components.client.clone(),
+            transaction_pool: service_components.transaction_pool.clone(),
+            spawn_handle: service_components.task_manager.spawn_handle(),
+            import_queue: service_components.import_queue,
             block_announce_validator_builder: None,
             warp_sync_params: None,
             block_relay: None,
         })?;
 
-    let (sync_oracle, _) = SyncOracle::new();
-
-    let validator_address_cache = match collect_extra_debugging_data {
-        true => Some(ValidatorAddressCache::new()),
-        false => None,
-    };
-
+    let chain_status = SubstrateChainStatus::new(service_components.backend.clone())
+        .map_err(|e| ServiceError::Other(format!("failed to set up chain status: {e}")))?;
+    let validator_address_cache = get_validator_address_cache(&aleph_config);
     let rpc_builder = {
-        let client = client.clone();
-        let pool = transaction_pool.clone();
+        let client = service_components.client.clone();
+        let pool = service_components.transaction_pool.clone();
         let sync_oracle = sync_oracle.clone();
         let validator_address_cache = validator_address_cache.clone();
+        let import_justification_tx = service_components.other.0.get_sender();
+        let chain_status = chain_status.clone();
         Box::new(move |deny_unsafe, _| {
             let deps = RpcFullDeps {
                 client: client.clone(),
@@ -267,159 +362,52 @@ fn setup(
         })
     };
 
-    let rpc_handlers = sc_service::spawn_tasks(sc_service::SpawnTasksParams {
+    let _rpc_handlers = sc_service::spawn_tasks(sc_service::SpawnTasksParams {
         network: network.clone(),
         sync_service: sync_network.clone(),
-        client,
-        keystore: keystore_container.local_keystore(),
-        task_manager,
-        transaction_pool,
+        client: service_components.client.clone(),
+        keystore: service_components.keystore_container.local_keystore(),
+        task_manager: &mut service_components.task_manager,
+        transaction_pool: service_components.transaction_pool.clone(),
         rpc_builder,
-        backend,
+        backend: service_components.backend,
         system_rpc_tx,
         tx_handler_controller,
         config,
-        telemetry: telemetry.as_mut(),
+        telemetry: service_components.other.1.as_mut(),
     })?;
 
-    let substrate_network = SubstrateNetwork::new(network, sync_network, protocol_naming);
-
-    Ok((
-        rpc_handlers,
-        substrate_network,
-        network_starter,
-        sync_oracle,
-        validator_address_cache,
-    ))
-}
-
-/// Builds a new service for a full client.
-pub fn new_authority(
-    config: Configuration,
-    aleph_config: AlephCli,
-) -> Result<TaskManager, ServiceError> {
-    let sc_service::PartialComponents {
-        client,
-        backend,
-        mut task_manager,
-        import_queue,
-        keystore_container,
-        select_chain,
-        transaction_pool,
-        other: (justification_channel_provider, mut telemetry, metrics),
-    } = new_partial(&config)?;
-
-    let (block_import, block_rx) = RedirectingBlockImport::new(client.clone());
-
-    let backup_path = backup_path(&aleph_config, config.base_path.path());
-
-    let finalized = client.info().finalized_hash;
-
-    let session_period = SessionPeriod(client.runtime_api().session_period(finalized).unwrap());
-
-    let millisecs_per_block =
-        MillisecsPerBlock(client.runtime_api().millisecs_per_block(finalized).unwrap());
-
-    let force_authoring = config.force_authoring;
-    let backoff_authoring_blocks = Some(LimitNonfinalized(aleph_config.max_nonfinalized_blocks()));
-    let prometheus_registry = config.prometheus_registry().cloned();
-
-    let import_queue_handle = BlockImporter::new(import_queue.service());
-
-    let chain_status = SubstrateChainStatus::new(backend.clone())
-        .map_err(|e| ServiceError::Other(format!("failed to set up chain status: {e}")))?;
-
-    let collect_extra_debugging_data = !aleph_config.no_collection_of_extra_debugging_data();
-
-    let (_rpc_handlers, substrate_network, network_starter, sync_oracle, validator_address_cache) =
-        setup(
-            config,
-            backend,
-            chain_status.clone(),
-            &keystore_container,
-            import_queue,
-            transaction_pool.clone(),
-            &mut task_manager,
-            client.clone(),
-            &mut telemetry,
-            justification_channel_provider.get_sender(),
-            collect_extra_debugging_data,
-        )?;
-
-    let mut proposer_factory = sc_basic_authorship::ProposerFactory::new(
-        task_manager.spawn_handle(),
-        client.clone(),
-        transaction_pool.clone(),
-        prometheus_registry.as_ref(),
-        None,
-    );
-    proposer_factory.set_default_block_size_limit(MAX_BLOCK_SIZE as usize);
-
-    let slot_duration = sc_consensus_aura::slot_duration(&*client)?;
-
-    let aura = sc_consensus_aura::start_aura::<AuraPair, _, _, _, _, _, _, _, _, _, _>(
-        StartAuraParams {
-            slot_duration,
-            client: client.clone(),
-            select_chain: select_chain.clone(),
-            block_import,
-            proposer_factory,
-            create_inherent_data_providers: move |_, ()| async move {
-                let timestamp = sp_timestamp::InherentDataProvider::from_system_time();
-
-                let slot =
-                    sp_consensus_aura::inherents::InherentDataProvider::from_timestamp_and_slot_duration(
-                        *timestamp,
-                        slot_duration,
-                    );
-
-                Ok((slot, timestamp))
-            },
-            force_authoring,
-            backoff_authoring_blocks,
-            keystore: keystore_container.local_keystore(),
-            sync_oracle: sync_oracle.clone(),
-            justification_sync_link: (),
-            block_proposal_slot_portion: SlotProportion::new(2f32 / 3f32),
-            max_block_proposal_slot_portion: None,
-            telemetry: telemetry.as_ref().map(|x| x.handle()),
-            compatibility_mode: Default::default(),
-        },
-    )?;
-
-    task_manager
+    service_components
+        .task_manager
         .spawn_essential_handle()
         .spawn_blocking("aura", None, aura);
 
-    if aleph_config.external_addresses().is_empty() {
-        panic!("Cannot run a validator node without external addresses, stopping.");
-    }
+    let rate_limiter_config = get_rate_limit_config(&aleph_config);
 
-    let rate_limiter_config = RateLimiterConfig {
-        alephbft_bit_rate_per_connection: aleph_config
-            .alephbft_bit_rate_per_connection()
-            .try_into()
-            .unwrap_or(usize::MAX),
-    };
-
+    let substrate_network = SubstrateNetwork::new(network, sync_network, protocol_naming);
     // Network event stream needs to be created before starting the network,
     // otherwise some events might be missed.
     let network_event_stream = substrate_network.event_stream();
 
+    let AlephRuntimeVars {
+        millisecs_per_block,
+        session_period,
+    } = get_aleph_runtime_vars(&service_components.client);
+
     let aleph_config = AlephConfig {
         network: substrate_network,
         network_event_stream,
-        client,
+        client: service_components.client,
         chain_status,
         import_queue_handle,
-        select_chain,
+        select_chain: service_components.select_chain,
         session_period,
         millisecs_per_block,
-        spawn_handle: task_manager.spawn_handle().into(),
-        keystore: keystore_container.local_keystore(),
-        justification_channel_provider,
+        spawn_handle: service_components.task_manager.spawn_handle().into(),
+        keystore: service_components.keystore_container.local_keystore(),
+        justification_channel_provider: service_components.other.0,
         block_rx,
-        metrics,
+        metrics: service_components.other.2,
         registry: prometheus_registry,
         unit_creation_delay: aleph_config.unit_creation_delay(),
         backup_saving_path: backup_path,
@@ -428,15 +416,14 @@ pub fn new_authority(
         rate_limiter_config,
         sync_oracle,
         validator_address_cache,
-        transaction_pool,
+        transaction_pool: service_components.transaction_pool,
     };
 
-    task_manager.spawn_essential_handle().spawn_blocking(
-        "aleph",
-        None,
-        run_validator_node(aleph_config),
-    );
+    service_components
+        .task_manager
+        .spawn_essential_handle()
+        .spawn_blocking("aleph", None, run_validator_node(aleph_config));
 
     network_starter.start_network();
-    Ok(task_manager)
+    Ok(service_components.task_manager)
 }

commit a9935749cf2c92e32f712e959e5b567a8a88980b

A0-0000: Fix for gathering validator network data (#1903)
# Description

Fix for regression introduced in
https://github.com/Cardinal-Cryptography/aleph-node/commit/1d65f62de7e090d07b0e6f2b2261c3edaa9583a1#diff-787215c6ec15e1ecc63e5d3628fc9cce8a083c8c658f7ce2312ff0c5b0265d5eR219.

There would be a companion PR in `main` as well.

## Type of change

Please delete options that are not relevant.

- Bug fix (non-breaking change which fixes an issue)

# Testing

Run locally command:
```
 curl  -H "Content-Type: application/json" -d '{"id":1, "jsonrpc":"2.0", "method": "alephNode_unstable_validatorNetworkInfo", "params": []}' http://localhost:9944/
{"jsonrpc":"2.0","result":{"5GBNeWRhZc2jXu7D55rBimKYDk8PGk8itRYFTPfC8RJLKG5o":{"session":0,"network_level_address":"127.0.0.1:30344","validator_network_peer_id":"5FyuCn5rKTJdYDPTbT1Xhr8VjqYWYskNrudefpk4VQSpkQse"},"5DiDShBWa1fQx6gLzpf3SFBhMinCoyvHM1BWjPNsmXS8hkrW":{"session":0,"network_level_address":"127.0.0.1:30347","validator_network_peer_id":"5Ebkjsh9i9PyFYXDVxxXwPjwRSq9XQR2mJkgZoU5k9dX4ACp"},"5F4H97f7nQovyrbiq4ZetaaviNwThSVcFobcA5aGab6167dK":{"session":0,"network_level_address":"127.0.0.1:30346","validator_network_peer_id":"5CC71TV5oztBx3585vqrSEqQpeX8xvavjjBDy8ufAf7WA9CS"},"5Dfis6XL8J2P6JHUnUtArnFWndn62SydeP8ee8sG2ky9nfm9":{"session":0,"network_level_address":"127.0.0.1:30345","validator_network_peer_id":"5CroYKMd4ZCLkyf4oYGUZuB5XPswUDL4n69ptPpAMbHRfe6F"}},"id":1}⏎                                                         
```

bin/node/src/service.rs
@@ -211,9 +211,7 @@ fn get_aleph_runtime_vars(client: &Arc<FullClient>) -> AlephRuntimeVars {
 }
 
 fn get_validator_address_cache(aleph_config: &AlephCli) -> Option<ValidatorAddressCache> {
-    aleph_config
-        .no_collection_of_extra_debugging_data()
-        .then(ValidatorAddressCache::new)
+    (!aleph_config.no_collection_of_extra_debugging_data()).then(ValidatorAddressCache::new)
 }
 
 fn get_proposer_factory(