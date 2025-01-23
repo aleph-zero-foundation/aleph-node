d85f16ecaf6a38eafed38880bf6f42a331a8fb30
Marcin <marcin.radecki@cardinals.cc>
2025-01-23 14:57:29 +0100

    A0-4583: Make NonTransfer filter out identity pallet (#1918)
    Also, this PR sets proper `spec-version` - should be 15.0.0 as last cut
    off release branch is `release-14`.

--- bin/runtime/src/lib.rs
+++ bin/runtime/src/lib.rs
@@ -80,7 +80,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("aleph-node"),
     impl_name: create_runtime_str!("aleph-node"),
     authoring_version: 1,
-    spec_version: 16_000_000,
+    spec_version: 15_000_000,
     impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 19,
@@ -860,7 +860,6 @@ impl InstanceFilter<RuntimeCall> for ProxyType {
                     | RuntimeCall::Utility(..)
                     | RuntimeCall::Multisig(..)
                     | RuntimeCall::NominationPools(..)
-                    | RuntimeCall::Identity(..)
             ),
             ProxyType::Staking => {
                 matches!(
